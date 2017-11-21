cdbece4f882f4b687a0865920fc6217b6419a1e5
sugarac <uscwangxu@gmail.com>
2017-11-20 16:39:09 -0800

    201. Bitwise AND of Numbers Range

---
+++ src/Other/BitwiseAndOfNumbersRange.java
@@ -0,0 +1,23 @@
+package Other;
+
+public class BitwiseAndOfNumbersRange {
+    /**
+     * Solution1: O(logn); O(1)
+     * In one word, this problem is asking us to find the common prefix of m and n 's binary code.
+     * Shirt m and n right by 1 bit until they are equal and use a variable record the reduced multiple.
+     * Return multiply m by the multiple.
+     */
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) {
+            return 0;
+        }
+
+        int multiple = 1;
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            multiple <<= 1;
+        }
+        return m * multiple;
+    }
+}
